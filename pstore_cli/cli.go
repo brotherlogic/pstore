49675b1426484c2a7517172d4861159ca82ad9b3
Simon <brotherlogic@gmail.com>
2024-10-25 06:23:57 -0700

    Adds in a CLI element

---
+++ pstore_cli/cli.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	pbps "github.com/brotherlogic/pstore/proto"
+
+	"github.com/brotherlogic/goserver/utils"
+)
+
+func main() {
+	ctx, cancel := utils.ManualContext("pstore-cli", time.Hour)
+	defer cancel()
+
+	conn, err := utils.LFDial(os.Args[1])
+	if err != nil {
+		log.Fatalf("Bad dial: %v", err)
+	}
+
+	client := pbps.NewPStoreServiceClient(conn)
+
+	result, err := client.Read(ctx, &pbps.ReadRequest{Key: "sync/testing"})
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+	log.Printf("%v", result)
+}
